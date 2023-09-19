ee4ec784f6d0e3716da95abdaa3eaf46357ef5da
tsukahar <52079978+tsukahar@users.noreply.github.com>
2023-09-19 09:50:43 -0400

    Align fastq files with Bowtie2

---
+++ Bowtie2.py
@@ -0,0 +1,125 @@
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-1_CGCTCATT-AGGCTATA_S166_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-1_CGCTCATT-AGGCTATA_S166_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/DMSO_1h_Bic_0h_rep1.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-8_TCCGCGAA-GTCAGTAC_S185_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-8_TCCGCGAA-GTCAGTAC_S185_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/DMSO_1h_Bic_0h_rep2.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-15_CTGAAGCT-TCAGAGCC_S172_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-15_CTGAAGCT-TCAGAGCC_S172_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/DMSO_1h_Bic_0h_rep3.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-2_GAGATTCC-GCCTCTAT_S177_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-2_GAGATTCC-GCCTCTAT_S177_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/DMSO_4h_Bic_0h_rep1.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-9_TCTCGCGC-AGGCTATA_S186_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-9_TCTCGCGC-AGGCTATA_S186_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/DMSO_4h_Bic_0h_rep2.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-16_TAATGCGC-CTTCGCCT_S173_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-16_TAATGCGC-CTTCGCCT_S173_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/DMSO_4h_Bic_0h_rep3.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-3_ATTCAGAA-AGGATAGG_S180_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-3_ATTCAGAA-AGGATAGG_S180_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/DMSO_4h_water_0h_rep1.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-10_AGCGATAG-GCCTCTAT_S167_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-10_AGCGATAG-GCCTCTAT_S167_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/DMSO_4h_water_0h_rep2.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-17_CGGCTATG-TAAGATTA_S174_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-17_CGGCTATG-TAAGATTA_S174_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/DMSO_4h_water_0h_rep3.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-4_GAATTCGT-TCAGAGCC_S181_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-4_GAATTCGT-TCAGAGCC_S181_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_1h_Bic_0h_rep1.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-11_TCCGGAGA-GTCAGTAC_S168_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-11_TCCGGAGA-GTCAGTAC_S168_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_1h_Bic_0h_rep2.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-18_TCCGCGAA-ACGTCCTG_S175_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-18_TCCGCGAA-ACGTCCTG_S175_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_1h_Bic_0h_rep3.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-6_TAATGCGC-TAAGATTA_S183_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-6_TAATGCGC-TAAGATTA_S183_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_1h_water_0h_rep1.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-13_ATTCAGAA-GCCTCTAT_S170_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-13_ATTCAGAA-GCCTCTAT_S170_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_1h_water_0h_rep2.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-20_TCCGGAGA-AGGCTATA_S178_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-20_TCCGGAGA-AGGCTATA_S178_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_1h_water_0h_rep3.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-5_CTGAAGCT-CTTCGCCT_S182_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-5_CTGAAGCT-CTTCGCCT_S182_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_4h_Bic_0h_rep1.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-12_GAGATTCC-AGGCTATA_S169_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-12_GAGATTCC-AGGCTATA_S169_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_4h_Bic_0h_rep2.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-19_TCTCGCGC-GTCAGTAC_S176_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-19_TCTCGCGC-GTCAGTAC_S176_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_4h_Bic_0h_rep3.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-7_CGGCTATG-ACGTCCTG_S184_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-7_CGGCTATG-ACGTCCTG_S184_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_4h_water_0h_rep1.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-14_GAATTCGT-AGGATAGG_S171_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-14_GAATTCGT-AGGATAGG_S171_R2_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_4h_water_0h_rep2.sam
+
+bowtie2 -p 12 -q --local \
+-x /nfs/value/siwase2/GENOME_INDICES/MM10/Bowtie2/MM10 \
+-1 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-21_CGCTCATT-GCCTCTAT_S179_R1_001.fastq.gz \
+-2 /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2020_input_fastq/2002-MP-21_CGCTCATT-GCCTCTAT_S179_R1_001.fastq.gz \
+-S /nfs/dataden/umms-siwase/SHIGEKI/Takao_Bru-seq/2021-12-26_Bowtie2/MM401_4h_water_0h_rep3.sam
